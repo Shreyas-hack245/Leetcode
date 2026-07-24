b5b9db9ddb38140dee5098d7952bbe5afcb10b25
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-07-24 21:30:49 +0530

    Nested Array Generator

---
+++ javascript/Nested_Array_Generator.js
@@ -0,0 +1,42 @@
+/* 2649. Nested Array Generator
+
+Given a multi-dimensional array of integers, return a generator object which yields integers in the same order as inorder traversal.
+
+A multi-dimensional array is a recursive data structure that contains both integers and other multi-dimensional arrays.
+
+inorder traversal iterates over each array from left to right, yielding any integers it encounters or applying inorder traversal to any arrays it encounters.
+
+ 
+
+Example 1:
+
+Input: arr = [[[6]],[1,3],[]]
+Output: [6,1,3]
+Explanation:
+const generator = inorderTraversal(arr);
+generator.next().value; // 6
+generator.next().value; // 1
+generator.next().value; // 3
+generator.next().done; // true
+Example 2:
+
+Input: arr = []
+Output: []
+Explanation: There are no integers so the generator doesn't yield anything.*/
+
+//solution:
+
+var inorderTraversal = function*(arr) {
+    for (const item of arr) {
+        if (Array.isArray(item)) {
+            yield* inorderTraversal(item);
+        } else {
+            yield item;
+        }
+    }
+};
+
+const gen = inorderTraversal([1, [2, 3]]);
+gen.next().value; 
+gen.next().value; 
+gen.next().value; 
