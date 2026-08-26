0ff8646dda50c362dd28bd622e1f665f4c82852b
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-08-26 21:24:55 +0530

    18. 4Sum

---
+++ dart/4Sum.dart
@@ -0,0 +1,52 @@
+/* 18. 4Sum
+
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.*/
+
+//solution:
+
+class Solution {
+  List<List<int>> fourSum(List<int> nums, int target) {
+    List<List<int>> result = [];
+    int n = nums.length;
+    
+     if (n < 4) return result;
+    
+     nums.sort();
+    
+     for (int i = 0; i < n - 3; i++) {
+       if (i > 0 && nums[i] == nums[i - 1]) continue;
+      
+       for (int j = i + 1; j < n - 2; j++) {
+         if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        
+         int left = j + 1;
+        int right = n - 1;
+        
+        while (left < right) {
+          int sum = nums[i] + nums[j] + nums[left] + nums[right];
+          
+          if (sum == target) {
+            result.add([nums[i], nums[j], nums[left], nums[right]]);
+            
+             while (left < right && nums[left] == nums[left + 1]) left++;
+             while (left < right && nums[right] == nums[right - 1]) right--;
+            
+             left++;
+            right--;
+          } else if (sum < target) {
+            left++;  
+          } else {
+            right--; 
+          }
+        }
+      }
+    }
+    
+    return result;
+  }
+}
